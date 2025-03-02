020e329765570eb9bf1ff57d49da4fee8ff0b5ef
Christian Ashley <christianzashley@gmail.com>
2025-03-02 11:11:18 -0500

    Set up JUnit
    Credit to RIT SWEN601 assignments

---
+++ tests/BoxGameTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BoxGameTest {
+  @Test
+  public void setUp() {
+    assertTrue(true);
+  }
+}
